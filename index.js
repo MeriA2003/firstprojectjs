20c288cdd2741e4b0a6d1585dfbfd195f36e118e
MeriA2003 <meri.altunyan@edu.ysu.am>
2024-10-21 20:44:08 +0400

    index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-	const inputText = document.querySelector('#txt');//գտնում է այն էլեմնտները որոնց id-ն txt է
+	const inputText = document.querySelector('#txt');
 	const myButton = document.querySelector('.list');
 	const list = document.querySelector('.div ul');
 	myButton.addEventListener('click', function(e)
 		{
-			e.preventDefault(); //եթե սա չգրենք չի ավելացնում ՝ բառացի կանխում է լռելյայն ձևի ներկայացումը կամ կոճակի վարքագիծը
-			//Եթե ինփութը դատարկ է
+			e.preventDefault(); 
 			if (inputText.value === "") {
 				alert("Դաշտը լրացված չէ։ Խնդրում ենք լրացնել!"); 
 				 
 			}
-			//եթե ինփութըդատարկ չէ 
 			else{
-					const myLi = document.createElement('li');//ստեղծում է էլեմենտ լի
-					myLi.innerHTML = inputText.value;//ինփութի արժեքնը վերցնում է
-					list.appendChild(myLi);//ինփութից վերցրած արժեքը կցում է ցուցակին
+					const myLi = document.createElement('li');
+					myLi.innerHTML = inputText.value;
+					list.appendChild(myLi);
 					const mySpan = document.createElement('span');
 					mySpan.innerHTML = 'Delete';
 					myLi.appendChild(mySpan);
-					//երբ դելեթ սեղմենք ցուցակից տարրը հանի
 					const close = document.querySelectorAll('span');
 					for (let i = 0; i < close.length; i++) {
 						close[i].addEventListener('click', function() {
 							close[i].parentElement.style.opacity = 0;
 							setTimeout(function() {
 								close[i].parentElement.remove();
-							}, 500);//500մլվրկ սպասում է և ջնջում է էլեմնետը ցուցակից
+							}, 500);
 						});
 					}
 					
